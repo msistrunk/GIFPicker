788cb0df59f6ecbde9482325054806da768177d1
Mitchell Sistrunk <sistrunkmitchell@gmail.com>
2020-11-09 20:56:29 -0600

    Remove default components and create search input component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <SearchInput />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchInput from './components/SearchInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchInput
   }
 }
 </script>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    input[type=text] {
+        border-radius:10px;
+        font-size:20pt;
+    }
+</style>

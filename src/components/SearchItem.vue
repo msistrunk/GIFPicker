5c1eb957cbd736308c6fd5273108ff2a6bebec26
Mitchell Sistrunk <sistrunkmitchell@gmail.com>
2020-11-11 14:36:20 -0600

    Add copy link to clipboard on click of gif

--- src/components/SearchItem.vue
+++ src/components/SearchItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='item'>
+  <div class='item' v-on:click='copyToClipboard(item.images.original.mp4)'>
     <video class='video' width=200 height=200 autoplay loop :title=item.title playsInline muted> 
       <source :src=item.images.original.mp4 type="video/mp4">
     </video>
@@ -17,18 +17,28 @@ export default {
       copyText: 'Copy',
     }
   },
+  methods: {
+    copyToClipboard(str) {
+      const el = document.createElement('textarea');
+      el.value = str;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.video{
-  object-fit: cover;
-  border-radius: 10px;
-}
 .item{
   position: relative;
   cursor: pointer;
   margin: 5px 5px 0px 5px
+}
+.video{
+  object-fit: cover;
+  border-radius: 10px;
 }
 </style>
